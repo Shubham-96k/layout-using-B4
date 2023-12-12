8e8bc2f6843cadd1826b1a3b6391d8a17134c359
Shubham-96k <iamcoder8989@gmail.com>
2023-12-13 00:26:47 +0530

    worked on homepage

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const cl = console.log;
+
+const toggleicon = document.getElementById("toggle");
+
+
+const onclick = eve => {
+    eve.target.classList.toggle("fa-bars");
+    eve.target.classList.toggle("fa-xmark");
+}
+
+toggleicon.addEventListener("click", onclick);
