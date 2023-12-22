7d86ad8deb7c851608bfcff5280c429faa47c149
Shubham-96k <iamcoder8989@gmail.com>
2023-12-22 16:28:14 +0530

    worked on aboutus page and owlCarousel.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,25 @@ const onclick = eve => {
     eve.target.classList.toggle("fa-xmark");
 }
 
-
+$('#testimonialsOwlCarousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    navText: ['<i class="fa-solid fa-chevron-left"></i> Prev' , 'Next <i class="fa-solid fa-chevron-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
 
 toggleicon.addEventListener("click", onclick);
